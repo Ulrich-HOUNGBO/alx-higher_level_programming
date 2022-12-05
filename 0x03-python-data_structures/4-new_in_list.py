a3cb27a5768f142e933ee36c15b28106e56bd916
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2022-12-05 22:57:10 +0100

    function that replaces an element in a list at a specific position without modifying the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    new_list = my_list
-    if idx < 0:
-        return new_list
-    elif idx >= len(my_list):
-        return new_list
-    else:
-        new_list[idx] = element
-        return new_list
-    
+def print_reversed_list_integer(my_list=[]):
+    if my_list is not None:
+        for i in my_list[::-1]:
+            print("{:d}".format(i))
