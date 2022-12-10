b0180106a293c7dfbbdbfeafe40e010bbb746aa2
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2022-12-10 01:33:45 +0100

    replace or add key/value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if a_dictionary is not None:
+        new_dictionary = a_dictionary
+        new_dictionary[key] = value
+        return new_dictionary
+ 
