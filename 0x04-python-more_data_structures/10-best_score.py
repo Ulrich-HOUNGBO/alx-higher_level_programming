3050d00faa97ff120ca5591cd516398f9920c6fa
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2022-12-10 02:01:11 +0100

    returns a key with the biggest integer value in dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        best = 0
+        person = None
+        for i in a_dictionary:
+            if a_dictionary[i] > best:
+                best = a_dictionary[i]
+                person = i
+        return person
