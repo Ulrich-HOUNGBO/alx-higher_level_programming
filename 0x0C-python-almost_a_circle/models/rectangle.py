7880b4d2842674f3f2bc8a286123916a3ace36e0
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2023-01-13 17:21:25 +0100

    adding the public method def display(self):

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""""""
 Base = __import__('base.py').Base
 
 
@@ -10,63 +10,71 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
-        
+
     @property
     def width(self):
         return self.__width
-    
+
     @property
     def height(self):
-        return self.__height 
-    
+        return self.__height
+
     @property
     def x(self):
         return self.__x
-    
-    @property 
+
+    @property
     def y(self):
         return self.__y
-    
+
     @width.setter
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be a integer")
-        
+
         if value <= 0:
             raise ValueError("width must be > 0")
-        
+
         self.width = value
-        
+
     @height.setter
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be a integer")
-        
+
         if value <= 0:
             raise ValueError("height must be > 0")
-        
+
         self.height = value
-        
+
     @x.setter
     def x(self, value):
         if type(value) is not int:
             raise TypeError("x must be a integer")
-        
-        if value <= 0: 
+
+        if value <= 0:
             raise ValueError("x must be > 0")
-        
+
         self.x = value
-        
+
     @y.setter
     def y(self, value):
         if type(value) is not int:
             raise TypeError("x must be a integer")
-        
-        if value <= 0: 
+
+        if value <= 0:
             raise ValueError("x must be > 0")
-        
+
         self.y = value
-        
+
     def area(self):
         return self.__width * self.__height
-    
+
+    def display(self):
+        print("\n" * self.__y +
+              "\n".join(" " * self.__x + "#" * self.__width
+                        for i in range(self.__height)))
+
+    def __str__(self):
+        return "[{:s}] ({:d}) {:d} / {:d} - {:d} / {:d}".format(self.__class__.name, self.id, self.__x,
+                                                                self.__y, self.__width, self.__height)
