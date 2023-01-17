c819f5b6ebd7bb36627e18765edbdac30b3364cf
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2023-01-17 03:02:58 +0100

    update Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+import json
+import csv
+
+
 class Base:
     __nb_objects = 0
 
@@ -8,3 +12,49 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            list_dictionaries = []
+
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or len(json_string) == 0:
+            json_string = "[]"
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        objc = []
+        if list_objs is not None:
+            for o in list_objs:
+                objc.append(cls.to_dictionary(o))
+                filename = cls.__name__ + ".json"
+                with open(filename, "w") as f:
+                    f.write(cls.to_json_string(objc))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns instance with attributes already set"""
+        if cls.__name__ == "Square":
+            dum = cls(1)
+        if cls.__name__ == "Rectangle":
+            dum = cls(1, 1)
+        dum.update(**dictionary)
+        return dum
+
+    @classmethod
+    def load_from_file(cls):
+        filename = cls.__name__ + ".json"
+        instance_list = []
+        try:
+            with open(filename, "r") as f:
+                instances = cls.from_json_string(f.read())
+            for i, dic in enumerate(instances):
+                instance_list.append(cls.create(**instances[i]))
+        except:
+            pass
+        return instance_list
