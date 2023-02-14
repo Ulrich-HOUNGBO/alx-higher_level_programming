b2e38fa7092954951429c02b439041d52c27c42e
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2023-02-14 06:20:58 +0100

    computes the score average of all records in the table

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- computes the score average of all records in the table
 -- result column name should be 'average'
- SELECT AVG(score) AS 'average' FROM secondçtable;
+ SELECT AVG(score) AS 'average' FROM second_table;
