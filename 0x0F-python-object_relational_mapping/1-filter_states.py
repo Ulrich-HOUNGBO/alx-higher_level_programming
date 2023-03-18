dbda3c05b017c09ba03ccdaf6d1d7ff7fa3d01aa
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2023-03-18 15:47:01 +0100

    script that lists all states with a name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,23 +3,23 @@
 return all table values (table 'states') that statting with N
 parameters given to script: username, password, database
 """
-import MYSQLdb
 from sys import argv
 
+import MySQLdb
+
 if __name__ == '__main__':
-    #connect to database
-    db=MYSQLdb.connect(host="localhost",
-                        port=3306,
-                        user=argv[1],
-                        passwd=argv[2],
-                        db=argv[3])
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     # create cursor to exec queries using SQL; filter names starting with 'N'
-    cur= db.cursor()
+    cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
-        if row[1][0]='N':
+        if row[1][0] == 'N':
             print(row)
-    cur.close() 
+    cur.close()
     db.close()
-
