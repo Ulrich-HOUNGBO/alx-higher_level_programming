bdcc0108ea2357f784ef67a51277b2227fa01e59
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2023-03-18 19:12:40 +0100

    script that takes in an argument and displays all values in the states table

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,23 +3,27 @@
 return matching states
 parameters given to script: username, password, database, state to match
 """
+
 import MySQLdb
 from sys import argv
 
-if __name == '__main__':
+if __name__ == "__main__":
+
     # connect to database
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3])
-    # create cursor to execute queries
-    cur = db.cursor()
-    sql_cmd = """SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY is ASC""".format(argv[4])
-    cur.execute(sql_cmd)
-    rows = cur.fetchall()
-    for row in rows:
+
+    # create cursor to exec queries using SQL; match arg given
+    cursor = db.cursor()
+    sql_cmd = """SELECT *
+                 FROM states
+                 WHERE name LIKE '{:s}' ORDER BY id ASC""".format(argv[4])
+    cursor.execute(sql_cmd)
+    for row in cursor.fetchall():
         if row[1] == argv[4]:
             print(row)
-    cur.close()
+    cursor.close()
     db.close()
