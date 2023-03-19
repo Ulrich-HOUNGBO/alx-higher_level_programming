a0511dd0485b7a3f867c5f12e01bd63bb84f5137
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2023-03-19 19:27:10 +0100

    fixe error

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 """
-return info from both tables (tables 'cities' 'states')
+return info from both tables (tables 'cities' 'states)
 parameters given to script: username, password, database
 """
+
 from sys import argv
 
 import MySQLdb
 
-if __name__ == '__main__':
-    # database connection
+if __name__ == "__main__":
+
+    # connect to database
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3])
-    # create cursor to execute queries
+
+    # create cursor to exec queries using SQL; join two tables for all info
     cur = db.cursor()
-    sql_cmd = """SELECT cities.id, cities.name, state.name FROM states 
-                 INNER JOIN cities ON state.id = cities.states_id
+    sql_cmd = """SELECT cities.id, cities.name, states.name
+                 FROM states
+                 INNER JOIN cities ON states.id = cities.state_id
                  ORDER BY cities.id ASC"""
     cur.execute(sql_cmd)
     rows = cur.fetchall()
