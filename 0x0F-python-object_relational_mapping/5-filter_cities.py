119615da5ddd73bfdfec4c7050ca826008c80a2f
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2023-03-19 19:34:48 +0100

    script that takes in the name of a state as an argument and lists all cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-return all cities from state given in argument
+return cities that are in the state given (tables 'cities' 'states)
 parameters given to script: username, password, database, state
 """
 
@@ -9,22 +9,24 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    # database connection
+    # connect to database
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3])
-    # create cursor to execute queries
-    cur = db.cursor()
-    sql_cmd = """SELECT cities.name FROM cities 
+
+    # create cursor to exec queries using SQL; join two tables for all info
+    cursor = db.cursor()
+    sql_cmd = """SELECT cities.name
+                 FROM states
                  INNER JOIN cities ON states.id = cities.state_id
-                 WHERE states.name LIKE %s 
+                 WHERE states.name LIKE %s
                  ORDER BY cities.id ASC"""
-    cur.execute(sql_cmd)
-    rows = cur.fetchall()
+    cursor.execute(sql_cmd, (argv[4],))
+
     # format the printing of cities of same state separated by commas
-    for row in rows:
-        print(', '.join(["{:s}".format(row[0])]))
-    cur.close()
+    print(', '.join(["{:s}".format(row[0]) for row in cursor.fetchall()]))
+
+    cursor.close()
     db.close()
