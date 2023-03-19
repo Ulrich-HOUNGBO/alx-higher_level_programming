b289f46493439dbba2f3a115a201adaee529ab74
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2023-03-19 20:24:46 +0100

    fixe error

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
-list all state objects from database that contain the letter a
-parameters given to script: mysql username, mysql password, database name
+return state objects containing letter 'a' from database via python
+parameters given to script: username, password, database
 """
+
 from sys import argv
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from model_state import State, Base
+from model_state import State
 
 if __name__ == "__main__":
+
     # make engine for database
     user = argv[1]
     passwd = argv[2]
@@ -20,8 +22,9 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # query python instance with python
-    for instance in session.query(State).filter(State.name.like('%a%')). \
-            order_by(State.id).frist():
-        print("{:d}: {:s}".format(instance.id, instance.name))
+    # query python instance in database for letter 'a'
+    for state in session.query(State).filter(
+            State.name.like('%a%')).order_by(State.id):
+        print("{:d}: {:s}".format(state.id, state.name))
+
     session.close()
