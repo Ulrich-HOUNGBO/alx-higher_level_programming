45e0ab230ca7bf3377fb47d1e193299c146accc6
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2023-04-01 01:39:51 +0100

    sends a request to the URL and displays the body of the response

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+sends a request to the URL and displays the body of the response
+"""
+
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.error
+    import sys
+
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
