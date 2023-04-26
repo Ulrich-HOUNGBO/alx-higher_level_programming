3fa6cc5bb54e0d155c0c9d2493cb5f0381039c0e
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2023-04-26 02:56:45 +0100

    script that gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,11 +4,10 @@ const url = process.argv[2];
 const filename = process.argv[3];
 const fs = require('fs');
 const request = require('request');
-
 request(url, function (err, response, body) {
-  fs.writeFile(filename, body, 'utf8', function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
+    fs.writeFile(filename, body, 'utf8', function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
 });
